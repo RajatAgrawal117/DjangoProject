36501f7822e156251367ec9394c8435ed21b60b4
Rajat Agrawal <rajat.ace22@sot.pdpu.ac.in>
2024-09-25 02:42:07 +0530

    Exercise 5: Data persistence and retrieval and improved the UI

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('submit_form/', views.submit_form, name='submit_form'),
+    path('submissions/', views.submissions_view, name='submissions'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import FormSubmission
 from datetime import datetime
-from django.http import HttpResponse
+
 def home(request):
     current_time = datetime.now()
     return render(request, 'home.html', {'current_time': current_time})
 
-
 def submit_form(request):
     if request.method == 'POST':
-        name = request.POST['name']
-        email = request.POST['email']
-        return HttpResponse(f"Received: {name}, {email}")
-    return HttpResponse("Invalid method.")
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+
+        submission = FormSubmission(name=name, email=email)
+        submission.save()
+
+        return redirect('submissions')
+
+    return render(request, 'form.html')
+
+def submissions_view(request):
+ 
+    submissions = FormSubmission.objects.all()
+    return render(request, 'submissions.html', {'submissions': submissions})
