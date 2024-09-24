56d63ecf8f5428d5796e07dcaff1639b523a2c5e
Rajat Agrawal <rajat.ace22@sot.pdpu.ac.in>
2024-09-25 02:15:48 +0530

    Exercise 4: Form handling for user details

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('submit_form/', views.submit_form, name='submit_form'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from datetime import datetime
-
+from django.http import HttpResponse
 def home(request):
     current_time = datetime.now()
     return render(request, 'home.html', {'current_time': current_time})
+
+
+def submit_form(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        return HttpResponse(f"Received: {name}, {email}")
+    return HttpResponse("Invalid method.")
