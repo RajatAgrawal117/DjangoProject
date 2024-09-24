df276cadb3bbd074d010a8937d9794867f84cfbe
Rajat Agrawal <rajat.ace22@sot.pdpu.ac.in>
2024-09-25 01:40:18 +0530

    Exercise 3: Added dynamic content (date/time)

--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from datetime import datetime
 
 def home(request):
-    return render(request, 'home.html')
+    current_time = datetime.now()
+    return render(request, 'home.html', {'current_time': current_time})
